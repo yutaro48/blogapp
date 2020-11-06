2de110fe03a265393f8267e560a1cf8ad0c9de0f
Horino Yutaro <suzy.city16@gmail.com>
2020-11-07 08:51:07 +0900

    Day20:ArticlesテーブルにUser_idのcolumnを追加、UserとArticleが紐づいている事をmodelで表現

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_084334) do
+ActiveRecord::Schema.define(version: 2020_11_06_232308) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
