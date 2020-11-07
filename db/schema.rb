6e1e528b9f9e8080ec86cdfa40548ec6451c7f72
Horino Yutaro <suzy.city16@gmail.com>
2020-11-07 14:30:19 +0900

    Day21:migrationファイルをrollbackして編集してmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_232308) do
+ActiveRecord::Schema.define(version: 2020_10_29_035351) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
