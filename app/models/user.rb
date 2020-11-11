5ab9d71a572ba593eecfd18ea9decc1e83ead520
Horino Yutaro <suzy.city16@gmail.com>
2020-11-11 12:12:13 +0900

    Day25:いいねした記事一覧の表示/トップページに戻るボタンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :favorite_articles, through: :likes, source: :article
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
