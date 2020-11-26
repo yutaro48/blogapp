2553345566575b20890401a8417b0b5661e612b6
Horino Yutaro <suzy.city16@gmail.com>
2020-11-26 20:21:06 +0900

    フォロー関係を作るためのモデルの修正(User/Relationship)

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: `Relationship`, dependent: :destroy
+  has_many :followings, through: following_relationships, source: :following
+
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
