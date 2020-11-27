ac4aac35d46754bcf5654a67bf35a9fab9033031
Horino Yutaro <suzy.city16@gmail.com>
2020-11-27 11:08:47 +0900

    フォロー機能の実装 routing→follows_controller→view

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,13 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    following_relationships.create!(following_id: user.id)
+    if user.is_a?(User)
+      user_id = user.id
+    else
+      user_id = user
+    end
+
+    following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
