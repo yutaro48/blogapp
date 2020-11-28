2ddad1ca088b3ee479971f4207ba2385483552e3
Horino Yutaro <suzy.city16@gmail.com>
2020-11-28 22:30:08 +0900

    JSでlikeのステータスによって表示を切り替える

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,11 +22,20 @@ require("@rails/actiontext")
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+    if (hasLiked) {
+        $('.active-heart').removeClass('hidden')
+      } else {
+        $('.inactive-heart').removeClass('hidden')
+      }
+}
+
 document.addEventListener('turbolinks:load', () => {
     const dataset = $('#article-show').data()
     const articleId = dataset.articleId
     axios.get(`/articles/${articleId}/like`)
       .then((response) => {
-          console.log(response)
+          const hasLiked = response.data.hasLiked
+          handleHeartDisplay(hasLiked)
       })
   })
