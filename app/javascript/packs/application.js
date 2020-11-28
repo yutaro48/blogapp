756d31b863915a394a15e51764aad0d7ca77f0c2
Horino Yutaro <suzy.city16@gmail.com>
2020-11-28 23:27:40 +0900

    APIを作ってそこにリクエストを投げたら画面遷移せずにいいねできるようにする

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,9 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 const handleHeartDisplay = (hasLiked) => {
     if (hasLiked) {
@@ -37,5 +40,27 @@ document.addEventListener('turbolinks:load', () => {
       .then((response) => {
           const hasLiked = response.data.hasLiked
           handleHeartDisplay(hasLiked)
+      })
+
+    $('.inactive-heart').on('click', () => {
+      axios.post(`/articles/${articleId}/like`)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((e) => {
+          window.alert('Error')
+          console.log(e)
+        })
+    })
+
+    $('.active-heart').on('click', () => {
+        axios.delete(`/articles/${articleId}/like`)
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+          })
       })
   })
