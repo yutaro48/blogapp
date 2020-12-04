d83c84d7e6b835c171d59fc7a4b5a2beb6915910
Horino Yutaro <suzy.city16@gmail.com>
2020-12-04 19:31:40 +0900

    commentのダミーデータを作成して、commentのGETリクエストをテスト

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Comments", type: :request do
-  describe "GET /api/comments" do
-    it "works! (now write some real specs)" do
-      get api_comments_path
+RSpec.describe 'Api::Comments', type: :request do
+  let!(:user) { create(:user) }
+  let!(:article) { create(:article, user: user) }
+  let!(:comment) { create_list(:comment, 3, article: article) }
+
+  describe 'GET /api/comments' do
+    it '200ステータスが返ってくる' do
+      get api_comments_path(article_id: article.id)
       expect(response).to have_http_status(200)
     end
   end
