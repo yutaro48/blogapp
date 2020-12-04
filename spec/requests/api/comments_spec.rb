dd73e5cc53b21f86734e87f2c045b4deaeaee052
Horino Yutaro <suzy.city16@gmail.com>
2020-12-04 19:47:10 +0900

    commentのAPIのレスポンスの内容（json）をチェックする

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Api::Comments', type: :request do
   let!(:user) { create(:user) }
   let!(:article) { create(:article, user: user) }
-  let!(:comment) { create_list(:comment, 3, article: article) }
+  let!(:comments) { create_list(:comment, 3, article: article) }
 
   describe 'GET /api/comments' do
     it '200ステータスが返ってくる' do
       get api_comments_path(article_id: article.id)
       expect(response).to have_http_status(200)
+
+      body = JSON.parse(response.body)
+      expect(body.length).to eq 3
+      expect(body[0]['content']).to eq comments.first.content
+      expect(body[1]['content']).to eq comments.second.content
+      expect(body[2]['content']).to eq comments.third.content
     end
   end
 end
