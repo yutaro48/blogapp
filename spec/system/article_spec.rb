58e86e50c001621eb287e07c238ba98b8a1c2fbf
Horino Yutaro <suzy.city16@gmail.com>
2020-12-06 14:15:52 +0900

    記事のタイトルが表示されているかの確認テスト

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Article', type: :system do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
   it '記事一覧が表示される' do
     visit root_path
-#root_pathのページを開きますというvisitメソッド
+    
+    articles.each do |article|
+      expect(page).to have_content(article.title)
+    end
   end
 end
